0063cca3871fba47cf25851e92dd3d6412ed19b5
saifur1681 <moonly1681@gmail.com>
2022-09-20 21:34:45 +0600

    login component addeded

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,25 +1,41 @@
 import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Login.css'
 
 const Login = () => {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    const handleEmailBlur = event => {
+        setEmail(event.target.value);
+    }
+
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value);
+    }
+
+    const handleUserLogIn = event => {
+        event.preventDefault();
+    }
+
     return (
         <div className='form-control'>
             <div>
                 <h1 className='form-title' >Login</h1>
-                <form>
+                <form onSubmit={handleUserLogIn} >
                     <div className='input-group'>
                         <label htmlFor="email">Email</label>
-                        <input type="email" name='email' required />
+                        <input onBlur={handleEmailBlur} type="email" name='email' required />
                     </div>
                     <div className='input-group'>
                         <label htmlFor="password">Password</label>
-                        <input type="password" name='password' required />
+                        <input onBlur={handlePasswordBlur} type="password" name='password' required />
                     </div>
                     <input className="form-submit-btn" type="submit" value="Login" />
                 </form>
                 <p className="link-container">
-                    New to Ema-John? <Link className="form-link"  to="/signup" >Create an account</Link>
+                    New to Ema-John? <Link className="form-link" to="/signup" >Create an account</Link>
                 </p>
             </div>
         </div>
