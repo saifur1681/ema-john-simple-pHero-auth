4888acda7b31919007a88bf1e14bc29de64f4783
saifur1681 <moonly1681@gmail.com>
2022-09-21 22:20:47 +0600

    lasst commit

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -10,7 +10,7 @@
  * if user is created redierect to the expected page
  * useSignInWithEmailAndPassword for sign in
  * Create RequireAuth Component ==> check user exists also track user location
- * in route wrap protected component by using Require Auth Component.
+ * in route wrap protected component by using Require Auth Component
  * 
  *  
  **/
